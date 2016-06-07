8f3050ed42533ac1e36f3f9d5e4c4fde84d801bf
Saurav Panta <spanta@telogical.com>
2016-06-07 18:40:14 -0500

    new ga module

--- src/web2/index.js
+++ src/web2/index.js
@@ -5,9 +5,24 @@ import App from './modules/App'
 import Home from './modules/Home'
 import Chapters from './modules/Chapters'
 import Chapter from './modules/Chapter'
+import ga from 'react-ga';
+ga.initialize('UA-78893348-1');
+function cleanpath(windowlocation){
+    var sanitized=windowlocation;
+    if(sanitized.indexOf('#')==-1){
+        return '/';
+    }
+    else{
+        sanitized=sanitized.substr(sanitized.indexOf('#')+1,sanitized.length-1);
+    }
+    return sanitized.split('?')[0];
+}
+function logPageView() {
+  ga.pageview(cleanpath(window.location.hash));
+}
 
 render((
-  <Router history={hashHistory}>
+  <Router history={hashHistory} onUpdate={logPageView}>
     <Route path="/" component={App}>
       <IndexRoute component={Chapters}/>
       <Route path="/chapters" component={Chapters}>
