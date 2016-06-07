b447c486b1cc9c2d4c2eb2658d98d7fbfeb602ce
Saurav Panta <spanta@telogical.com>
2016-06-07 17:57:10 -0500

    google analtyics

--- src/web2/modules/App.js
+++ src/web2/modules/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import NavLink from './NavLink'
+import GoogleAnalytics from 'react-g-analytics'
 
 export default React.createClass({
   render() {
     return (
       <div>
+        
+        <GoogleAnalytics id="UA-78893348-1" />
         <h1>विश्वगीता</h1>
         {this.props.children}
       </div>
